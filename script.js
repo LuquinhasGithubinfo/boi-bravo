09f078eb9c17aa85c663eb12d6af8840c3e04047
wvitecnologia <lucassimpat@gmail.com>
2025-12-30 18:16:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,9 @@
 // Pequenas melhorias futuras (scroll suave, analytics, etc.)
 console.log("Boi Bravo site carregado com sucesso.");
+
+function scrollParaBaixo() {
+  window.scrollBy({
+    top: window.innerHeight,
+    behavior: 'smooth'
+  });
+}
